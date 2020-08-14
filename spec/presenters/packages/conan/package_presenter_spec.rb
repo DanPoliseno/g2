10208ccccdce84185da37663673a7bca53293134
Steve Abrams <sabrams@gitlab.com>
2020-08-14 15:04:25 -0600

    Conan project-level API endpoints
    Adds a new set of Grape routes for the Conan
    package registry that is within the project
    resource.

--- spec/presenters/packages/conan/package_presenter_spec.rb
+++ spec/presenters/packages/conan/package_presenter_spec.rb
@@ -7,9 +7,10 @@ RSpec.describe ::Packages::Conan::PackagePresenter do
   let_it_be(:package) { create(:conan_package) }
   let_it_be(:project) { package.project }
   let_it_be(:conan_package_reference) { '123456789'}
+  let(:params) { { package_scope: :instance } }
 
   describe '#recipe_urls' do
-    subject { described_class.new(package, user, project).recipe_urls }
+    subject { described_class.new(package, user, project, params).recipe_urls }
 
     context 'no existing package' do
       let(:package) { nil }
@@ -30,6 +31,20 @@ RSpec.describe ::Packages::Conan::PackagePresenter do
       context 'when there are multiple channels for the same package' do
         let(:conan_metadatum) { create(:conan_metadatum, package_channel: 'newest' ) }
         let!(:newest_package) { create(:conan_package, name: package.name, version: package.version, project: project, conan_metadatum: conan_metadatum) }
+
+        it { is_expected.to eq(expected_result) }
+      end
+
+      context 'with package_scope of project' do
+        # #recipe_file_url checks for params[:id]
+        let(:params) { { id: project.id } }
+
+        let(:expected_result) do
+          {
+            "conanfile.py" => "#{Settings.build_base_gitlab_url}/api/v4/projects/#{project.id}/packages/conan/v1/files/#{package.conan_recipe_path}/0/export/conanfile.py",
+            "conanmanifest.txt" => "#{Settings.build_base_gitlab_url}/api/v4/projects/#{project.id}/packages/conan/v1/files/#{package.conan_recipe_path}/0/export/conanmanifest.txt"
+          }
+        end
 
         it { is_expected.to eq(expected_result) }
       end
@@ -59,10 +74,16 @@ RSpec.describe ::Packages::Conan::PackagePresenter do
 
   describe '#package_urls' do
     let(:reference) { conan_package_reference }
+    let(:params) do
+      {
+        conan_package_reference: reference,
+        package_scope: :instance
+      }
+    end
 
     subject do
       described_class.new(
-        package, user, project, conan_package_reference: reference
+        package, user, project, params
       ).package_urls
     end
 
@@ -82,6 +103,26 @@ RSpec.describe ::Packages::Conan::PackagePresenter do
       end
 
       it { is_expected.to eq(expected_result) }
+
+      context 'with package_scope of project' do
+        # #package_file_url checks for params[:id]
+        let(:params) do
+          {
+            conan_package_reference: reference,
+            id: project.id
+          }
+        end
+
+        let(:expected_result) do
+          {
+            "conaninfo.txt" => "#{Settings.build_base_gitlab_url}/api/v4/projects/#{project.id}/packages/conan/v1/files/#{package.conan_recipe_path}/0/package/#{conan_package_reference}/0/conaninfo.txt",
+            "conanmanifest.txt" => "#{Settings.build_base_gitlab_url}/api/v4/projects/#{project.id}/packages/conan/v1/files/#{package.conan_recipe_path}/0/package/#{conan_package_reference}/0/conanmanifest.txt",
+            "conan_package.tgz" => "#{Settings.build_base_gitlab_url}/api/v4/projects/#{project.id}/packages/conan/v1/files/#{package.conan_recipe_path}/0/package/#{conan_package_reference}/0/conan_package.tgz"
+          }
+        end
+
+        it { is_expected.to eq(expected_result) }
+      end
 
       context 'multiple packages with different references' do
         let(:info_file) { create(:conan_package_file, :conan_package_info, package: package) }
