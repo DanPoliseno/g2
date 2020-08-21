90d658a96d5f6a7f54a81a91078270978f3426de
Nick Thomas <nick@gitlab.com>
2020-08-21 16:50:35 +0100

    Sync LFS objects when push mirroring

--- spec/features/projects/remote_mirror_spec.rb
+++ spec/features/projects/remote_mirror_spec.rb
@@ -33,6 +33,26 @@ RSpec.describe 'Project remote mirror', :feature do
       end
     end
 
+    context 'pushing to a remote' do
+      let(:remote_project) { create(:project, :empty_repo) }
+
+      before do
+        remote_project.add_maintainer(user)
+
+        # FIXME: currently blocked
+        remote_mirror.update!(url: remote_project.http_url_to_repo)
+      end
+
+      it 'transfers code and LFS objects' do
+        lfs_object = create(:lfs_objects_project, project: project).lfs_object
+
+        Projects::UpdateRemoveMirrorService.new(project, user).execute(remote_mirror)
+
+        expect(remote_project.lfs_objects.reload.count).to eq(1)
+        expect(remote_project.commit).to eq(project.commit)
+      end
+    end
+
     def expect_mirror_to_have_error_and_timeago(timeago)
       row = first('.js-mirrors-table-body tr')
       expect(row).to have_content('Error')
