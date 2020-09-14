06358292ddecf72668cd588553e4f1cee5c51e8f
Tan Le <tle@gitlab.com>
2020-09-14 17:15:14 +1000

    Add CSV export button to Admin Audit Log
    Administrator can export audit events to CSV from Admin Audit Log
    page. This will use the current filter criteria for exporting.

---
+++ ee/app/assets/javascripts/audit_events/components/audit_events_export_button.vue
@@ -0,0 +1,42 @@
+<script>
+import { GlButton, GlTooltip } from '@gitlab/ui';
+import { __ } from '~/locale';
+
+export default {
+  components: {
+    GlButton,
+    GlTooltip,
+  },
+  props: {
+    exportHref: {
+      required: true,
+      type: String,
+    },
+  },
+  strings: {
+    buttonText: __('Current view'),
+    tooltipText: __('Export as CSV'),
+    tooltipSizeLimit: __('(max size 15 MB)'),
+  },
+  data() {
+    return {
+      button: null,
+    };
+  },
+  mounted() {
+    this.button = this.$refs.button;
+  },
+};
+</script>
+
+<template>
+  <div>
+    <gl-button ref="button" :href="exportHref" icon="export">
+      {{ $options.strings.buttonText }}
+    </gl-button>
+    <gl-tooltip v-if="button" :target="button" boundary="viewport" placement="top">
+      <p class="gl-my-0">{{ $options.strings.tooltipText }}</p>
+      <p class="gl-my-0">{{ $options.strings.tooltipSizeLimit }}</p>
+    </gl-tooltip>
+  </div>
+</template>
